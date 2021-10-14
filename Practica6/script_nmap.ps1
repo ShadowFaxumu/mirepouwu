42f5306d50225a347715263898194f8191bb77d6
Paulina Gómez <88333127+ShadowFaxumu@users.noreply.github.com>
2021-10-14 12:51:20 -0500

    Añadiendo script y resultados(practica6)

---
+++ Practica6/script_nmap.ps1
@@ -0,0 +1,40 @@
+﻿#Created by ShadowFax
+
+
+function Get-PublicIP {
+    $publicip = Invoke-WebRequest ifconfig.me | Select -ExpandProperty Content 
+    Write-Output "Your public ip is: "$publicip "`n"
+}
+
+
+function Get-LocalIP {
+    $localip = Get-NetIPAddress -AddressFamily IPv4 | Format-Table
+    Write-Output "Your local ip addresses are: "
+    Write-Output $localip
+} 
+
+
+function Use-ipnmap {
+    $privateip = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex 11 
+    $ipaddress = $privateip.IPAddress
+    $nmap = nmap.exe $ipaddress
+    Write-Output "An nmap was performed to "$ipaddress $nmap
+}
+
+function Use-urlnmap {
+    $url = 'www.instagram.com'
+    $nmap2 = nmap.exe $url
+    Write-Output "-----------------------------------------------------------"
+    Write-Output "`n" "An nmap was performed to "$url $nmap2
+}
+
+#Results are saved without coding
+Get-PublicIP | Out-File -FilePath ./resultados.txt 
+Get-LocalIP | Out-File -FilePath ./resultados.txt -Append
+Use-ipnmap | Out-File -FilePath ./resultados.txt -Append
+Use-urlnmap | Out-File -FilePath ./resultados.txt -Append
+
+#Encoding the file
+$file = ".\resultados.txt"
+[System.Convert]::ToBase64String((Get-Content $file -Encoding Byte)) | Out-File -FilePath ./results.txt 
+Write-Output "The results have been saved in a folder called 'results' in the current path"
